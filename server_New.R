8322dd1df4801297ed7f96cd550c51cc42fca1b5
Ali Arsalan <scac_1041@hotmail.com>
2014-08-29 14:28:56 +0300

    Create server_New.R

---
+++ server_New.R
@@ -0,0 +1,249 @@
+#######============================================= Basic Textual Analysis with R ==================================================================#######
+
+
+
+#==========================================================================================================================================================#
+##============================ Section 0: Load libraries & create custom functions =======================================================================##
+
+library(shiny)
+library(shinyIncubator)
+library(ggplot2)
+library(tm)
+library(fastcluster)
+library(gsl)
+library(topicmodels)
+
+
+
+
+cleanSweepCorpus<- function(corpus, useStopwords=FALSE, stem=FALSE, removePunct=FALSE, removeNum=FALSE, useSynonyms=FALSE,
+			    initialWords, replacementWords, useCustomStopwords=FALSE, customStopwords){
+  newCorpus <- corpus
+  newCorpus <- sapply(newCorpus, function(x) tolower(x))
+  newCorpus <- gsub("[\\\\()/]", " ", newCorpus)
+  if (useStopwords != FALSE){
+	engStopwords <- c(stopwords("SMART"),stopwords("english"))
+	engStopwords <- gsub("^","\\\\b",engStopwords)
+	engStopwords <- gsub("$","\\\\b",engStopwords)
+	x <- mapply(FUN= function(...){
+		newCorpus <<- gsub(..., replacement=" ", x=newCorpus)},
+		pattern=engStopwords)
+	rm(x)
+  }
+  if (useCustomStopwords != FALSE){
+	stopwords <- unlist(strsplit(customStopwords, split=","))
+	modifiedStopwords <- gsub("^[[:space:]]*", "\\\\b", stopwords)
+	modifiedStopwords <- gsub("[[:space:]]*$", "\\\\b", modifiedStopwords)
+	x <- mapply(FUN= function(...){
+		newCorpus <<- gsub(..., replacement=" ", x=newCorpus)},
+		pattern=modifiedStopwords)
+	rm(x)
+  }
+  if (useSynonyms != FALSE){
+	toChange <- unlist(strsplit(initialWords, split=","))
+	changeTo <- unlist(strsplit(replacementWords, split=","))
+	toChangeWords <- gsub("^[[:space:]]*", "\\\\b", toChange)
+	toChangeWords <- gsub("[[:space:]]*$", "\\\\b", toChangeWords)
+	changeToWords <- gsub("^[[:space:]]*", "\\\\b", changeTo)
+	changeTowords <- gsub("[[:space:]]*$", "\\\\b", changeToWords)
+	if (length(toChangeWords) == length(changeToWords)){
+		x <- mapply(FUN= function(...){
+		newCorpus <<- gsub(..., x=newCorpus)},
+		pattern=toChangeWords, replacement=changeToWords)
+		rm(x)
+	} else {
+		print("It appears that the number of replacing and to be replaced words are not the same...")
+	}
+  }
+  if (removePunct != FALSE){
+	newCorpus <- gsub("[[:punct:]]", " ", newCorpus)
+  }
+  if (removeNum != FALSE){
+	newCorpus <- gsub("[[:digit:]]", " ", newCorpus)
+  }
+  newCorpus <- stripWhitespace(newCorpus)
+  newCorpus <- gsub("^[[:space:]]*", "", newCorpus)
+  newCorpus <- gsub("[[:space:]]*$", "", newCorpus)
+  newCorpus <- gsub("[^[:alnum:]]", " ", newCorpus)
+  newCorpus <- newCorpus[which(newCorpus != "")]
+  if (stem != FALSE){
+	newCorpus <- sapply(newCorpus, wordStem)
+  }
+  finalCorpus <- Corpus(VectorSource(newCorpus))
+  finalCorpus <- tm_map(finalCorpus, stripWhitespace)
+  return(finalCorpus)
+}
+
+
+
+cosineDist<- function(x){
+x %*% t(x) / sqrt(rowSums(x^2) %*% t(rowSums(x^2))) }
+
+
+
+
+ghubURL <- "https://raw2.github.com/noobuseR/Datasets/master/"
+
+
+
+
+
+#======================================================================================================================#
+#============================= The Shiny application =================================================================##
+
+
+
+shinyServer(function(input,output,session){
+
+
+
+initialCorpus <- reactive({ 
+				if (input$uploadBtn == 0)
+				return()
+				isolate({
+					if (input$corpusType == "userCorpus"){
+						userUpload <- reactive ({ input$myPath })
+						userFilesPath <- userUpload()$datapath
+						myData <- unlist(lapply(userFilesPath, function(x) scan(file=x, what="character", sep="\n",
+						fileEncoding="UTF-8", encoding="UTF-8")))
+#					} else {
+#						myFile <- RCurl::getURL(url=paste0(ghubURL, input$choiceCorpus, ".txt"), ssl.verifypeer=FALSE)
+#						myData <- unlist(strsplit(myFile, split="\n"))
+					}
+					myCorpus <- Corpus(VectorSource(myData))
+					return(myCorpus)
+				})
+			})
+
+
+output$corpusStatus <- renderPrint({
+					if(input$uploadBtn == 0)
+					return("No corpus selection made yet...")
+					withProgress(session, {
+						setProgress(message = "Uploading your corpus...")
+						isolate({
+							initialCorpus()
+						})
+					})
+			})
+
+
+
+#========================================================================================================================#
+##============================= Section 2: Pre-processing ==================================================================##
+
+
+
+preprocessedCorpus <- reactive({
+					if(input$preProcessBtn == 0)
+					return()
+					isolate({
+						originalCorpus <- initialCorpus()
+#						newCorpus <- cleanSweepCorpus(corpus = originalCorpus, useStopwords = input$stopwords,
+#						stem = input$stemming, removePunct = input$punctuation, removeNum = input$numbers, useSynonyms = input$customThes,
+#						useCustomStopwords = input$customStopword, initialWords = input$customThesInitial,
+#						replacementWords = input$customThesReplacement, customStopwords = input$cusStopwords)
+#						rm(originalCorpus)
+#						return(newCorpus)
+						return(originalCorpus)
+					})
+			})
+
+
+output$procCorpusStatus <- renderPrint({
+						if(input$preProcessBtn == 0)
+						return("No pre-processing applied on Corpus...")
+						withProgress(session, {
+							setProgress(message = "Processing your corpus...")
+							isolate({
+								preprocessedCorpus() 
+							})
+					})
+			})
+
+
+
+#========================================================================================================================#
+#=========================== Section 3: Feature Generation, Weighting, and Selection ========================================##
+
+
+
+initialUnigramMatrix <- reactive({
+				if(input$generateMatrixBtn == 0)
+				return()
+				isolate({
+					corpus <- preprocessedCorpus()
+					weightingScheme <- paste0(input$termWeight, input$docWeight, input$normalisation)
+					initialMatrix <- TermDocumentMatrix(corpus,
+								control = list(weighting = function(x)weightSMART(x, spec = weightingScheme)))
+					rm(corpus)
+					return(initialMatrix) 
+				})
+			})
+
+
+output$initialuniMatrix <- renderPrint({ 
+				if(input$generateMatrixBtn == 0)
+				return("No Term-Document matrix constituted of single words available at the moment")
+				withProgress(session, {
+					setProgress(message = "Calculating your Term Document Matrix...")
+					isolate({initialUnigramMatrix() 
+					})
+				})
+			})
+
+lowerFreqRange <- reactive({
+				myMatrix <- initialUnigramMatrix()
+				freq <- rowSums(as.matrix(myMatrix))
+				range(freq)
+			})
+
+
+output$lowerFreqSlider<- renderUI({
+					sliderInput("lowerFreqBound", "Please set the Lower Bound for Frequency",
+					min = round(lowerFreqRange()[1]), max = round(lowerFreqRange()[2]), value = round(lowerFreqRange()[1]), 
+					step = NULL, ticks = TRUE)
+			})
+
+
+finalUnigramMatrix <- reactive({
+				if(input$selectFeaturesBtn == 0)
+				return()
+				isolate({
+					finalUnigramMatrix <- initialUnigramMatrix()
+					if(input$lowerFreqBound != lowerFreqRange()[1]){
+						lowerBound <- findFreqTerms(finalUnigramMatrix, round(input$lowerFreqBound), Inf)
+						finalUnigramMatrix <- finalUnigramMatrix[lowerBound,]}
+					if(input$sparsity != 100){
+						finalUnigramMatrix <- removeSparseTerms(finalUnigramMatrix, sparse = (input$sparsity/100))}
+					return(finalUnigramMatrix) 
+				})
+			})
+
+
+output$finaluniMatrix <- renderPrint({
+				if(input$selectFeaturesBtn == 0)
+				return("No Feature Selection procedure applied at the moment")
+				withProgress(session,{
+					setProgress(message = "Applying Feature Selection procedures...")
+					isolate({finalUnigramMatrix() 
+					})
+				})
+			})
+
+
+
+
+observe({
+	if(input$phase == "import" | input$phase == "preprocess" | input$phase == "featureGenerate" | input$phase == "featureSelect"){
+		updateTabsetPanel(session, "tabset1", "Corpus Generation")
+	} else if(input$phase == "about"){
+		updateTabsetPanel(session, "tabset1", "About")
+	} else if(input$phase == "userGuide"){
+		updateTabsetPanel(session, "tabset1", "User Guide")
+	}
+	})
+
+})
+
+
